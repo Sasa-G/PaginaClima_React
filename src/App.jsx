d304b6878681d3adca08f3e29ff7b582317d9bfb
Samanta Gomes De Morais Santos <samantagdms@gmail.com>
2024-05-21 00:18:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import PaginaClima from './Components/Pagina do Clima/PaginaClima'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <PaginaClima/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
